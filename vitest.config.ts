b49c418d0a185e117222046d392fc457460bf06d
Wayne Van Son <waynevanson@gmail.com>
2026-02-02 15:19:18 +1100

    build: ue vite plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
+import { cargo } from "@waynevanson/vite-plugin-cargo";
 import { defineConfig } from "vitest/config";
-import { cargo } from "./vite-plugin-cargo";
 
 export default defineConfig({
 	plugins: [cargo({ includes: "src/**/lib.rs" })],
