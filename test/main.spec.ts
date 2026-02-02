295dae9b1c1055620b7c08037feec35e3561778b
Wayne Van Son <waynevanson@gmail.com>
2026-02-02 15:53:38 +1100

    test: add coverage

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,8 +1,28 @@
-import { describe, test, expect } from "vitest";
+import { describe, expect, test } from "vitest";
 import { ArgMatches, Command } from "../src/lib.rs";
 
 describe("clap-js", () => {
-	test("constructor", () => {
-		expect(() => new Command("hello")).not.toThrow();
+	describe("construct", () => {
+		test("constructor", () => {
+			expect(() => new Command("")).not.toThrow();
+
+			const instance = new Command("");
+			expect(instance).toBeInstanceOf(Command);
+		});
+
+		test("from", () => {
+			expect(() => Command.default()).not.toThrow();
+
+			const instance = Command.default();
+			expect(instance).toBeInstanceOf(Command);
+		});
+	});
+
+	describe("instance", () => {
+		test("ArgMatches", () => {
+			const instance = Command.default();
+			const argMatches = instance.getMatches();
+			expect(argMatches).toBeInstanceOf(ArgMatches);
+		});
 	});
 });
